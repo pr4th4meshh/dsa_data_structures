830a0395b95169bdc5f130e1a7f5caa0ddbcaebd
pr4th4meshh <87704191+pr4th4meshh@users.noreply.github.com>
2023-11-28 17:36:46 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,24 @@
+//Sets
+// const set = new Set([1,2,3])
+// set.add(4)
+// set.add(4) //Won't be printed because sets cannot take duplicate value
+// console.log(set.has(4))
+// set.delete(3)
+// console.log("Size:", set.size)
+// set.clear()
+
+// for(const item of set) {
+//   console.log(item)
+// }
+
+//Maps
+// const map = new Map([["a", 1], ["b", 2], ["c", 3]])
+// map.set("d", 4) //adding new element
+// map.delete("d") //deleting an element
+// map.clear()
+// console.log(map.size)  //checking size of the map
+// console.log(map.has("a")) //checking an element
+
+// for(const [key, value] of map) {
+//   console.log(`${key} : ${value}`)
+// }
